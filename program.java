75ca3d58f45afaaa5b8bafa6c3b62bb35639be9e
Rohit Verma <verma.rohit.in@gmail.com>
2020-04-14 19:26:29 +0530

    compile program and run

---
+++ program.java
@@ -0,0 +1,10 @@
+class program
+{
+    public static void main( String args[]){
+        // System is a predefined class
+        // out is a variable
+        // print is a function
+        // name of the file is same as class name 
+        System.out.print("first program");
+    }
+}
