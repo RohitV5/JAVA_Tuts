13d49776e9c80468eac4da0fdea88a5f30a87204
Rohit Verma <verma.rohit.in@gmail.com>
2020-04-17 17:21:34 +0530

    Data type int

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 "# JAVA_Tuts" 
+# Keywords are case sensitive.
+
+# public class Hello {}
+# Main method is the entry point. Java program looks for this method to run.
+
+# int datatype are numbers without decimals
+# everything in double quotes is a string literal.
--- program.java
+++ program.java
@@ -1,10 +1,36 @@
-class program
+class program1
 {
     public static void main( String args[]){
         // System is a predefined class
         // out is a variable
         // print is a function
-        // name of the file is same as class name 
+        // javac will generate the class file same as your class name 
+
+        // a *= 4 ==> a = a * 4;
+        // pre increment y = ++x first increment then assign
+        // post increment y = x++  first assign then increment
         System.out.print("first program");
+
+        int myFirstNumber = 5;
+
+        System.out.print(myFirstNumber);
+        intDataType();
+    }
+
+    public static void intDataType(){
+        int maxValue = Integer.MAX_VALUE;
+        int minValue = Integer.MIN_VALUE;
+        System.out.print("Max value " + maxValue);
+        System.out.print("MIn value" + minValue);
+
+        //Overflow
+        System.out.print("Busted Max value" + (maxValue + 1));
+        System.out.print("Busted Min" + (minValue-1));
+
+
+        //ways to format int
+        int myMaxTest = 2_147_483_647;
+
+
     }
 }
